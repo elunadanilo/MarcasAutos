8a868ede858fa3f6cee7f49bf9a2339131a2cc6d
DaniloLuna <danilo.itzep-ext@dominion-global.com>
2026-03-13 12:11:36 -0600

    Se agrega clase para manejo de excepciones

---
+++ MarcasAutos/MarcasAutos.Domain/Exceptions/BusinessException.cs
@@ -0,0 +1,17 @@
+﻿namespace MarcasAutos.Domain.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        #region Public Constructors
+
+        public BusinessException()
+        {
+        }
+
+        public BusinessException(string message) : base(message)
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
