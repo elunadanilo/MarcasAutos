3ae7a91b6a5d8862ded3590b5bc085844008d61c
DaniloLuna <danilo.itzep-ext@dominion-global.com>
2026-03-13 12:05:03 -0600

    Se crea dto y mensajes de respuesta

---
+++ MarcasAutos/MarcasAutos/Responses/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MarcasAutos.Api.Responses
+{
+    public class ApiResponse<T>
+    {
+        public ApiResponse(T data)
+        {
+            Data = data;
+        }
+
+        public T Data { get; set; }
+    }
+}
