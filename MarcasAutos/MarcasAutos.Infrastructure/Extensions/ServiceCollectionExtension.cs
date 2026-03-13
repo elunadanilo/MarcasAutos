08bdc0ad911d3c5d9f8d4b564a6dcfdf32b30e85
DaniloLuna <danilo.itzep-ext@dominion-global.com>
2026-03-13 12:08:27 -0600

    Se crea inyeccion de dependencias

---
+++ MarcasAutos/MarcasAutos.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using MarcasAutos.Domain.Interfaces;
+using MarcasAutos.Infrastructure.Repositories;
+using MarcasAutos.Infrastructure.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarcasAutos.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Clase de extensión para registrar servicios y repositorios en la colección de servicios.
+    /// </summary>
+    public static class ServiceCollectionExtension
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Registra servicios y repositorios en la colección de servicios especificada.
+        /// </summary>
+        /// <param name="services">La colección de servicios IServiceCollection a la que se añadirán los servicios.</param>
+        /// <returns>La misma instancia de IServiceCollection después de que los servicios hayan sido añadidos. Esto permite encadenar múltiples llamadas de registro de servicios.</returns>
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //servicios
+            services.AddTransient<IMarcasAutosService, MarcasAutosService>();
+            //repositorios
+            services.AddTransient<IMarcasAutosRepository, MarcasAutosRepository>();
+
+            return services;
+        }
+
+        #endregion Public Methods
+    }
+}
