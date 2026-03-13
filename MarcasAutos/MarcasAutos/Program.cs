97be7fae8f731529a6960dea424ccbf528e6e185
DaniloLuna <danilo.itzep-ext@dominion-global.com>
2026-03-13 12:09:46 -0600

    Se crea configuracion de la aplicacion

--- MarcasAutos/MarcasAutos/Program.cs
+++ MarcasAutos/MarcasAutos/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AutoMapper;
+using MarcasAutos.Infrastructure.Context;
+using MarcasAutos.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configuring Entity Framework to use PostgreSQL with Npgsql as database provider
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ProjectDbContext>(opt =>
+{
+    opt.UseNpgsql(connectionString);
+});
+builder.Services.AddServices();
+
+var app = builder.Build();
+
+var mapper = app.Services.GetRequiredService<IMapper>();
+mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+// Performing database migrations at application startup
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetService<ProjectDbContext>();
+    context.Database.EnsureCreated();
+}
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
