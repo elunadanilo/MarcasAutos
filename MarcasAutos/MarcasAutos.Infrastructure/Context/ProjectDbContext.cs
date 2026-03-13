228787ee2569689f2a1e2147296aac33fbb9568b
DaniloLuna <danilo.itzep-ext@dominion-global.com>
2026-03-13 12:05:46 -0600

    Se crea base de datos y mappings

---
+++ MarcasAutos/MarcasAutos.Infrastructure/Context/ProjectDbContext.cs
@@ -0,0 +1,53 @@
+﻿using MarcasAutos.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace MarcasAutos.Infrastructure.Context
+{
+    /// <summary>
+    /// Contexto de base de datos para el proyecto.
+    /// </summary>
+    public class ProjectDbContext : DbContext
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Constructor de la clase ProjectDbContext.
+        /// </summary>
+        /// <param name="options">Opciones de configuración para el contexto.</param>
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Conjunto de datos para los vehículos de marca.
+        /// </summary>
+        public DbSet<MarcasAuto> MarcasAutos { get; set; }
+
+        #endregion Public Properties
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Configura el modelo mediante el ModelBuilder.
+        /// </summary>
+        /// <param name="modelBuilder">Constructor del modelo.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<MarcasAuto>().HasData(
+                new MarcasAuto { Id = 1, NombreMarca = "Toyota" },
+                new MarcasAuto { Id = 2, NombreMarca = "Mazda" },
+                new MarcasAuto { Id = 3, NombreMarca = "JAC" },
+                new MarcasAuto { Id = 4, NombreMarca = "Mitsubishi" },
+                new MarcasAuto { Id = 5, NombreMarca = "Datsun" }
+            );
+        }
+
+        #endregion Protected Methods
+    }
+}
---
+++ MarcasAutos/MarcasAutos.Infrastructure/Mappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MarcasAutos.Domain;
+using MarcasAutos.Domain.DTO;
+
+namespace MarcasAutos.Infrastructure.Mappings
+{
+    public class AutomapperProfile : Profile
+    {
+        #region Public Constructors
+
+        public AutomapperProfile()
+        {
+            CreateMap<MarcasAuto, MarcasAutosDto>();
+            CreateMap<MarcasAutosDto, MarcasAuto>();
+        }
+
+        #endregion Public Constructors
+    }
+}
