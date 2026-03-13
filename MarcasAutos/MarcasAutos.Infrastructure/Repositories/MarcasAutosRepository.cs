d8d3fd98619190f3b4f2a82d09c9da273a27a58e
DaniloLuna <danilo.itzep-ext@dominion-global.com>
2026-03-13 12:07:45 -0600

    Se crea repositorio y servicios

---
+++ MarcasAutos/MarcasAutos.Infrastructure/Repositories/MarcasAutosRepository.cs
@@ -0,0 +1,66 @@
+﻿using MarcasAutos.Domain;
+using MarcasAutos.Domain.Exceptions;
+using MarcasAutos.Domain.Interfaces;
+using MarcasAutos.Infrastructure.Context;
+using MarcasAutos.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarcasAutos.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Repositorio para las marcas de autos.
+    /// </summary>
+    public class MarcasAutosRepository : IMarcasAutosRepository
+    {
+        #region Private Fields
+
+        private readonly ProjectDbContext _projectDbContext;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Constructor de la clase MarcasAutosRepository.
+        /// </summary>
+        /// <param name="context">Contexto de la base de datos.</param>
+        public MarcasAutosRepository(ProjectDbContext context)
+        {
+            _projectDbContext = context;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Obtiene la lista de vehículos de una marca.
+        /// </summary>
+        /// <returns>Una tarea que representa la operación asincrónica. El resultado de la tarea contiene una colección de MarcasAutos.</returns>
+        public async Task<IEnumerable<MarcasAuto>> GetMarcasAutossAsyncRepository()
+        {
+            var listado = await _projectDbContext.MarcasAutos.ToListAsync();
+            return listado;
+        }
+
+        /// <summary>
+        /// Agrega un registro de MarcasAutos.
+        /// </summary>
+        /// <returns>Una tarea que representa la operación de agregado asincrónica</returns>
+        public async Task AddMarcaRepository(MarcasAuto brand)
+        {
+            try
+            {
+                _projectDbContext.Add(brand);
+                await _projectDbContext.SaveChangesAsync();
+            }
+            catch (Exception exc)
+            {
+
+                throw new BusinessException("Error al grabar encabezado de encuesta" + exc);
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ MarcasAutos/MarcasAutos.Infrastructure/Services/MarcasAutosService.cs
@@ -0,0 +1,53 @@
+﻿using MarcasAutos.Domain;
+using MarcasAutos.Domain.Interfaces;
+using MarcasAutos.Domain.Interfaces;
+
+namespace MarcasAutos.Infrastructure.Services
+{
+    /// <summary>
+    /// Servicio para las marcas de autos.
+    /// </summary>
+    public class MarcasAutosService : IMarcasAutosService
+    {
+        #region Private Fields
+
+        private readonly IMarcasAutosRepository _marcasAutosRepository;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Constructor de la clase MarcasAutosService.
+        /// </summary>
+        /// <param name="marcasAutosRepository">Repositorio de marcas de autos.</param>
+        public MarcasAutosService(IMarcasAutosRepository marcasAutosRepository)
+        {
+            _marcasAutosRepository = marcasAutosRepository;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Obtiene la lista de vehículos de una marca.
+        /// </summary>
+        /// <returns>Una tarea que representa la operación asincrónica. El resultado de la tarea contiene una colección de MarcasAutos.</returns>
+        public async Task<IEnumerable<MarcasAuto>> GetMarcasAutossAsyncService()
+        {
+            return await _marcasAutosRepository.GetMarcasAutossAsyncRepository();
+        }
+
+        /// <summary>
+        /// Servicio para agregar un registro de MarcasAutos.
+        /// </summary>
+        /// <returns>Una tarea que representa la operación de agregado asincrónica</returns>
+        public async Task AddMarcaService(MarcasAuto brand)
+        {
+            await _marcasAutosRepository.AddMarcaRepository(brand);
+        }
+
+        #endregion Public Methods
+    }
+}
